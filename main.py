c15c747d3ef60d33c08edbefcd8d26dcf60cdcdb
syam <syamgudipudi@Mac.ht.home>
2025-06-25 10:33:51 -0500

    Model training

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from Networksecurity.components.data_ingestion import DataIngestion
 from Networksecurity.components.data_validation import DataValidation
+
 from Networksecurity.exception.exception import NetworkSecurityException
 from Networksecurity.logging.logger import logging
-from Networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+
+from Networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from Networksecurity.entity.config_entity import TrainingPipelineConfig
 from Networksecurity.components.data_transformation import DataTransformation,DataTransformationConfig
+from Networksecurity.components.model_trainer import ModelTrainer
+
+from Networksecurity.entity.config_entity import ModelTrainerConfig
+from Networksecurity.components.data_transformation import DataTransformation
 import sys
 import os
 
 
 if __name__ == '__main__':
-    try:
+    try: 
         TrainingPipelineConfig = TrainingPipelineConfig()
         dataingestioconfig = DataIngestionConfig(TrainingPipelineConfig)
         data_ingestion = DataIngestion(dataingestioconfig)
@@ -32,6 +38,13 @@ if __name__ == '__main__':
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(TrainingPipelineConfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
 
 
 
